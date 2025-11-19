09d827803f87e85c65ece88d2378c18da6660460
KACHAN.21110 <KACHAN.21110@LABI1-13PC07>
2025-11-19 13:14:13 +0100

    Aggiungere i file di progetto.

---
+++ funzioni_costo/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace funzioni_costo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnProc = new System.Windows.Forms.Button();
+            this.listboxsp = new System.Windows.Forms.ListBox();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnProc
+            // 
+            this.btnProc.Location = new System.Drawing.Point(239, 26);
+            this.btnProc.Name = "btnProc";
+            this.btnProc.Size = new System.Drawing.Size(113, 36);
+            this.btnProc.TabIndex = 4;
+            this.btnProc.Text = "ok";
+            this.btnProc.UseVisualStyleBackColor = true;
+            this.btnProc.Click += new System.EventHandler(this.btnProc_Click);
+            // 
+            // listboxsp
+            // 
+            this.listboxsp.FormattingEnabled = true;
+            this.listboxsp.Location = new System.Drawing.Point(664, 12);
+            this.listboxsp.Name = "listboxsp";
+            this.listboxsp.Size = new System.Drawing.Size(432, 264);
+            this.listboxsp.TabIndex = 20;
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(432, 12);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(200, 200);
+            this.chart.TabIndex = 21;
+            this.chart.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1139, 539);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.listboxsp);
+            this.Controls.Add(this.btnProc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnProc;
+        private System.Windows.Forms.ListBox listboxsp;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+    }
+}
+
---
+++ funzioni_costo/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AngouriMath;
+
+namespace funzioni_costo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        double d(double q)
+        {
+            return 90 - 4 * q;
+        }
+        double o(double q)
+        {
+            return 10 + (q * q * q / 100);
+        }
+        private void btnProc_Click(object sender, EventArgs e)
+        {
+            double q = 0;
+            double diffsp = 1;
+            double maxsp = (float)1 / (float)1024;
+            while (diffsp>=maxsp)
+            {
+                var result_d = d(q);
+                var result_o = o(q);
+                if (!(result_d>result_o)) { 
+                q = q - diffsp; diffsp = diffsp / 2;listboxsp.Items.Add($"d = {result_d}, o = {result_o}, q- = {q - diffsp}, q+ = {q}"); }
+                q = q + diffsp;
+            }
+        }
+    }
+}
