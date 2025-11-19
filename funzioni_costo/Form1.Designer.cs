0414ecfdb669c5760bed17f8ae9a62f8c659289a
KACHAN.21110 <KACHAN.21110@LABI1-13PC07>
2025-11-19 13:59:03 +0100

    grafico

--- funzioni_costo/Form1.Designer.cs
+++ funzioni_costo/Form1.Designer.cs
@@ -28,23 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.btnProc = new System.Windows.Forms.Button();
             this.listboxsp = new System.Windows.Forms.ListBox();
             this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.labelPuntoEffic = new System.Windows.Forms.Label();
+            this.textBoxChartPrecision = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
             this.SuspendLayout();
             // 
             // btnProc
             // 
-            this.btnProc.Location = new System.Drawing.Point(31, 55);
+            this.btnProc.Location = new System.Drawing.Point(44, 41);
             this.btnProc.Name = "btnProc";
             this.btnProc.Size = new System.Drawing.Size(120, 36);
             this.btnProc.TabIndex = 4;
-            this.btnProc.Text = "Calcola";
+            this.btnProc.Text = "ok";
             this.btnProc.UseVisualStyleBackColor = true;
             this.btnProc.Click += new System.EventHandler(this.btnProc_Click);
             // 
@@ -58,16 +59,16 @@
             // 
             // chart
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart.Legends.Add(legend2);
-            this.chart.Location = new System.Drawing.Point(31, 292);
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(224, 41);
             this.chart.Name = "chart";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart.Series.Add(series2);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart.Series.Add(series1);
             this.chart.Size = new System.Drawing.Size(207, 200);
             this.chart.TabIndex = 21;
             this.chart.Text = "chart1";
@@ -75,16 +76,24 @@
             // labelPuntoEffic
             // 
             this.labelPuntoEffic.AutoSize = true;
-            this.labelPuntoEffic.Location = new System.Drawing.Point(457, 315);
+            this.labelPuntoEffic.Location = new System.Drawing.Point(457, 314);
             this.labelPuntoEffic.Name = "labelPuntoEffic";
             this.labelPuntoEffic.Size = new System.Drawing.Size(0, 13);
             this.labelPuntoEffic.TabIndex = 22;
+            // 
+            // textBoxChartPrecision
+            // 
+            this.textBoxChartPrecision.Location = new System.Drawing.Point(46, 92);
+            this.textBoxChartPrecision.Name = "textBoxChartPrecision";
+            this.textBoxChartPrecision.Size = new System.Drawing.Size(106, 20);
+            this.textBoxChartPrecision.TabIndex = 23;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1139, 539);
+            this.Controls.Add(this.textBoxChartPrecision);
             this.Controls.Add(this.labelPuntoEffic);
             this.Controls.Add(this.chart);
             this.Controls.Add(this.listboxsp);
@@ -102,6 +111,7 @@
         private System.Windows.Forms.ListBox listboxsp;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart;
         private System.Windows.Forms.Label labelPuntoEffic;
+        private System.Windows.Forms.TextBox textBoxChartPrecision;
     }
 }
 
--- funzioni_costo/Form1.cs
+++ funzioni_costo/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Antlr4.Runtime.Atn;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,7 +19,7 @@ namespace funzioni_costo
         {
             InitializeComponent();
         }
-        
+
         double d(double q)
         {
             return 90 - 4 * q;
@@ -32,20 +33,41 @@ namespace funzioni_costo
             // calcolo punto di equilibrio
             double q = 0;
             double diffsp = 1;
-            double maxsp = (float)1 / (float)65536; //precisione 1/2^16
+            double maxsp = (double)1 / (double)65536; //precisione 1/2^16
             double qResult = 0;
-            while (diffsp>=maxsp)
+            while (diffsp >= maxsp)
             {
                 var result_d = d(q);
                 var result_o = o(q);
-                if (!(result_d>result_o)) { 
-                q = q - diffsp; diffsp = diffsp / 2;listboxsp.Items.Add($"d = {result_d}, o = {result_o}, q- = {q - diffsp}, q+ = {q}"); qResult = (q+q-diffsp)/2;
+                if (!(result_d > result_o))
+                {
+                    q = q - diffsp; diffsp = diffsp / 2; listboxsp.Items.Add($"d = {result_d}, o = {result_o}, q- = {q - diffsp}, q+ = {q}"); qResult = (q + q - diffsp) / 2;
                 }
                 q = q + diffsp;
             }
             labelPuntoEffic.Text = $"Quantità di equilibrio: q = {qResult}, d = {d(qResult)}, o = {o(qResult)} -- precisione 1/2^16";
 
+            //
+            int precision = int.Parse(textBoxChartPrecision.Text);
+            maxsp = (double)1 / (double)Math.Pow(2, precision);
+            q = 0;
+            double qStop = 30;
+            chart.Series.Add("Domanda");
+            chart.Series["Domanda"].Color = Color.Brown;
+            chart.Series["Domanda"].ChartType = SeriesChartType.Point;
 
+            chart.Series.Add("Offerta");
+            chart.Series["Offerta"].Color = Color.Orange;
+            chart.Series["Offerta"].ChartType = SeriesChartType.Point;
+            while (q <= qStop)
+            {
+                var result_d = d(q);
+                var result_o = o(q);
+                chart.Series["Domanda"].Points.AddXY(q, result_d);
+                chart.Series["Offerta"].Points.AddXY(q, result_o);
+                q = q + maxsp;
+            
+            }
 
         }
     }
