2abd84b91103480b20106f0217b3fe31dc15e5bf
KACHAN.21110 <KACHAN.21110@LABI1-13PC07>
2025-11-26 12:53:56 +0100

    aggiunta possibilità di inserire le proprie funzioni attraverso i coefficienti (max. parametro q^3)

--- funzioni_costo/Form1.Designer.cs
+++ funzioni_costo/Form1.Designer.cs
@@ -28,25 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.btnProc = new System.Windows.Forms.Button();
             this.listboxsp = new System.Windows.Forms.ListBox();
             this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.labelPuntoEffic = new System.Windows.Forms.Label();
             this.textBoxChartPrecision = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.input_dq2 = new System.Windows.Forms.TextBox();
+            this.input_dq1 = new System.Windows.Forms.TextBox();
+            this.input_dq0 = new System.Windows.Forms.TextBox();
+            this.input_do0 = new System.Windows.Forms.TextBox();
+            this.input_do1 = new System.Windows.Forms.TextBox();
+            this.input_do2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.input_oq3 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.input_dq3 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
             this.SuspendLayout();
             // 
             // btnProc
             // 
-            this.btnProc.Location = new System.Drawing.Point(505, 83);
+            this.btnProc.Location = new System.Drawing.Point(372, 83);
             this.btnProc.Name = "btnProc";
             this.btnProc.Size = new System.Drawing.Size(120, 36);
             this.btnProc.TabIndex = 4;
-            this.btnProc.Text = "ok";
+            this.btnProc.Text = "calcola";
             this.btnProc.UseVisualStyleBackColor = true;
             this.btnProc.Click += new System.EventHandler(this.btnProc_Click);
             // 
@@ -60,16 +76,16 @@
             // 
             // chart
             // 
-            chartArea4.Name = "ChartArea1";
-            this.chart.ChartAreas.Add(chartArea4);
-            legend4.Name = "Legend1";
-            this.chart.Legends.Add(legend4);
+            chartArea3.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart.Legends.Add(legend3);
             this.chart.Location = new System.Drawing.Point(44, 149);
             this.chart.Name = "chart";
-            series4.ChartArea = "ChartArea1";
-            series4.Legend = "Legend1";
-            series4.Name = "Series1";
-            this.chart.Series.Add(series4);
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart.Series.Add(series3);
             this.chart.Size = new System.Drawing.Size(383, 350);
             this.chart.TabIndex = 21;
             this.chart.Text = "chart1";
@@ -94,15 +110,160 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(41, 95);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.Size = new System.Drawing.Size(265, 13);
             this.label1.TabIndex = 24;
-            this.label1.Text = "Precisione (2^(-n). inserire n)";
+            this.label1.Text = "Precisione (2^(-n). inserire n) preferibilmente minore di 8";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(213, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 13);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "q +";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(156, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "q^2 +";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "d =";
+            // 
+            // input_dq2
+            // 
+            this.input_dq2.Location = new System.Drawing.Point(132, 27);
+            this.input_dq2.Name = "input_dq2";
+            this.input_dq2.Size = new System.Drawing.Size(26, 20);
+            this.input_dq2.TabIndex = 31;
+            // 
+            // input_dq1
+            // 
+            this.input_dq1.Location = new System.Drawing.Point(187, 27);
+            this.input_dq1.Name = "input_dq1";
+            this.input_dq1.Size = new System.Drawing.Size(26, 20);
+            this.input_dq1.TabIndex = 32;
+            // 
+            // input_dq0
+            // 
+            this.input_dq0.Location = new System.Drawing.Point(231, 27);
+            this.input_dq0.Name = "input_dq0";
+            this.input_dq0.Size = new System.Drawing.Size(26, 20);
+            this.input_dq0.TabIndex = 33;
+            // 
+            // input_do0
+            // 
+            this.input_do0.Location = new System.Drawing.Point(231, 53);
+            this.input_do0.Name = "input_do0";
+            this.input_do0.Size = new System.Drawing.Size(26, 20);
+            this.input_do0.TabIndex = 39;
+            // 
+            // input_do1
+            // 
+            this.input_do1.Location = new System.Drawing.Point(187, 53);
+            this.input_do1.Name = "input_do1";
+            this.input_do1.Size = new System.Drawing.Size(26, 20);
+            this.input_do1.TabIndex = 38;
+            // 
+            // input_do2
+            // 
+            this.input_do2.Location = new System.Drawing.Point(132, 53);
+            this.input_do2.Name = "input_do2";
+            this.input_do2.Size = new System.Drawing.Size(26, 20);
+            this.input_do2.TabIndex = 37;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(213, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 36;
+            this.label4.Text = "q +";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(156, 56);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 35;
+            this.label6.Text = "q^2 +";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(41, 56);
+            this.label7.Name = "label7";
+            this.label7.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label7.Size = new System.Drawing.Size(22, 13);
+            this.label7.TabIndex = 34;
+            this.label7.Text = "o =";
+            // 
+            // input_oq3
+            // 
+            this.input_oq3.Location = new System.Drawing.Point(69, 53);
+            this.input_oq3.Name = "input_oq3";
+            this.input_oq3.Size = new System.Drawing.Size(26, 20);
+            this.input_oq3.TabIndex = 43;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(98, 56);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 13);
+            this.label8.TabIndex = 42;
+            this.label8.Text = "q^3 +";
+            // 
+            // input_dq3
+            // 
+            this.input_dq3.Location = new System.Drawing.Point(69, 27);
+            this.input_dq3.Name = "input_dq3";
+            this.input_dq3.Size = new System.Drawing.Size(26, 20);
+            this.input_dq3.TabIndex = 41;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(98, 30);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(34, 13);
+            this.label9.TabIndex = 40;
+            this.label9.Text = "q^3 +";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1139, 539);
+            this.Controls.Add(this.input_oq3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.input_dq3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.input_do0);
+            this.Controls.Add(this.input_do1);
+            this.Controls.Add(this.input_do2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.input_dq0);
+            this.Controls.Add(this.input_dq1);
+            this.Controls.Add(this.input_dq2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxChartPrecision);
             this.Controls.Add(this.labelPuntoEffic);
@@ -124,6 +285,22 @@
         private System.Windows.Forms.Label labelPuntoEffic;
         private System.Windows.Forms.TextBox textBoxChartPrecision;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox input_dq2;
+        private System.Windows.Forms.TextBox input_dq1;
+        private System.Windows.Forms.TextBox input_dq0;
+        private System.Windows.Forms.TextBox input_do0;
+        private System.Windows.Forms.TextBox input_do1;
+        private System.Windows.Forms.TextBox input_do2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox input_oq3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox input_dq3;
+        private System.Windows.Forms.Label label9;
     }
 }
 
--- funzioni_costo/Form1.cs
+++ funzioni_costo/Form1.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -19,27 +20,54 @@ namespace funzioni_costo
         {
             InitializeComponent();
         }
-
+        double dq2, dq1, dq0, oq2, oq1, oq0, dq3, oq3 = 0;
         double d(double q)
         {
-            return 90 - 4 * q;
+            return dq3 * q*q*q + dq2 * q*q + dq1 * q + dq0;
         }
         double o(double q)
         {
-            return 10 + (q * q * q / 100);
+            return oq3 * q*q*q + oq2 * q*q + oq1 * q + oq0;
         }
         private void btnProc_Click(object sender, EventArgs e)
         {
             // calcolo punto di equilibrio
             
-            if (!int.TryParse(textBoxChartPrecision.Text, out int precision)) { MessageBox.Show("inserire un numero"); return; }
+            if (!int.TryParse(textBoxChartPrecision.Text, out int precision)) { MessageBox.Show("inserire un numero per Precisione"); return; }
             if (chart.Series.IndexOf("Domanda") != -1) chart.Series.Remove(chart.Series["Domanda"]);
             if (chart.Series.IndexOf("Offerta") != -1) chart.Series.Remove(chart.Series["Offerta"]);
+            listboxsp.Items.Clear();
+
+            var r_dq3 = input_dq3.Text;
+            if (r_dq3 == "") dq3 = 0;
+            else if (!double.TryParse(r_dq3, out dq3)) { MessageBox.Show("errore dati: dq3"); return; }
+            var r_dq2 = input_dq2.Text;
+            if (r_dq2 == "") dq2 = 0;
+            else if (!double.TryParse(r_dq2, out dq2)) { MessageBox.Show("errore dati: dq2"); return; }
+            var r_dq1 = input_dq1.Text;
+            if (r_dq1 == "") dq1 = 0;
+            else if (!double.TryParse(r_dq1, out dq1)) { MessageBox.Show("errore dati: dq1"); return; }
+            var r_dq0 = input_dq0.Text;
+            if (r_dq0 == "") dq0 = 0;
+            else if (!double.TryParse(r_dq0, out dq0)) { MessageBox.Show("errore dati: dq0"); return; }
+            var r_oq3 = input_oq3.Text;
+            if (r_oq3 == "") oq3 = 0;
+            else if (!double.TryParse(r_oq3, out oq3)) { MessageBox.Show("errore dati: oq3"); return; }
+            var r_do2 = input_do2.Text;
+            if (r_do2 == "") oq2 = 0;
+            else if (!double.TryParse(r_do2, out oq2)) { MessageBox.Show("errore dati: oq2"); return; }
+            var r_do1 = input_do1.Text;
+            if (r_do1 == "") oq1 = 0;
+            else if (!double.TryParse(r_do1, out oq1)) { MessageBox.Show("errore dati: oq1"); return; }
+            var r_do0 = input_do0.Text;
+            if (r_do0 == "") oq0 = 0;
+            else if (!double.TryParse(r_do0, out oq0)) { MessageBox.Show("errore dati: oq0"); return; }
 
             double maxsp = (double)1 / (double)Math.Pow(2, precision);
             double q = 0;
             double diffsp = 1;
             double qResult = 0;
+
             while (diffsp >= maxsp)
             {
                 var result_d = d(q);
